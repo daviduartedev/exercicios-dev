2d07299dadf7a8bb25407b1ed50bd202e2334279
daviduartedev <daviduarte.dev@gmail.com>
2025-07-11 21:40:16 -0300

    exercicios de tipos e variaveis

---
+++ variaveisetipos.js
@@ -0,0 +1,19 @@
+// Declare uma variável do tipo string chamada nome e atribua seu nome a ela
+
+// Declare uma constante chamada idade com valor 23
+
+// Declare uma variável chamada isEstudando que armazena true ou false
+
+// Crie uma função que retorna a frase: "Meu nome é X e tenho Y anos", usando as variáveis acima
+
+// Execute a função e imprima o resultado no console
+
+var nome = 'Davi';
+
+const idade = 23;
+
+function meuNome (nome, idade) {
+    return `Meu nome é ${nome} e tenho ${idade} anos`;
+}
+
+console.log(meuNome(nome, idade));
