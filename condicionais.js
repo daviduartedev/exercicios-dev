43802b1fe1b4ada32c17b422c38ad35cebdf91a7
daviduartedev <daviduarte.dev@gmail.com>
2025-07-12 00:32:35 -0300

    exercicions de condicionais

---
+++ condicionais.js
@@ -0,0 +1,17 @@
+const idade = 18;
+
+if (idade >= 18) {
+    console.log('Parabéns, você é maior de idade')
+} else {
+    console.log('Espere até fazer 18 anos')
+}
+
+var turno = 'manha';
+
+if (turno === 'manha') { 
+    console.log('Bom dia!')
+} else if (turno === 'tarde') {
+    console.log('Boa tarde!')
+} else if (turno === 'noite') {
+    console.log('Boa noite')
+}
